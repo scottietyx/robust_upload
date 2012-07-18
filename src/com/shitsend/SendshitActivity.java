0c0f8a18515a6803bec043c9e86c74e0141eb9e4
David Shen <daft@MBP>
2012-07-18 11:17:57 -0400

    did a lot of stuff and it probably doesnt work

--- src/com/shitsend/SendshitActivity.java
+++ src/com/shitsend/SendshitActivity.java
@@ -1,35 +1,51 @@
 package com.shitsend;
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.StrictMode;
 import android.util.Log;
 
 public class SendshitActivity extends Activity {
 
-	private Map<String, String> id_data = new HashMap<String, String>();
+	// private Map<String, String> id_data = new HashMap<String, String>();
 
-	/** Called when the activity is first created. 
-	 * @return */
+	/**
+	 * Called when the activity is first created.
+	 * 
+	 * @return
+	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		if (android.os.Build.VERSION.SDK_INT > 9) {
-			StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+			StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+					.permitAll().build();
 			StrictMode.setThreadPolicy(policy);
 		}
 
@@ -37,165 +53,235 @@ public class SendshitActivity extends Activity {
 
 	}
 
+	int timestamp = 0;
+	
+	private class getLineTask extends AsyncTask<Void, Void, Void> {
+
+		//int timestamp = 0;
+		
+		@Override
+		protected Void doInBackground(Void... params) {
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpPost httppost = new HttpPost(
+					"http://dolan.bouncemen.net/robust_timestamp.php");
+
+			try {
+				// Add your data
+				List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(
+						2);
+				nameValuePairs.add(new BasicNameValuePair("sensor", "1"));
+				nameValuePairs.add(new BasicNameValuePair("id", "0"));
+				httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+				// Execute HTTP Post Request
+				HttpResponse response = httpclient.execute(httppost);
+
+				BufferedReader in = new BufferedReader(new InputStreamReader(
+						response.getEntity().getContent()));
+				StringBuffer sb = new StringBuffer("");
+				String line = "";
+				String NL = System.getProperty("line.separator");
+				while ((line = in.readLine()) != null) {
+					sb.append(line + NL);
+				}
+				in.close();
+				String page = sb.toString();
+				System.out.println(page);
+
+				if (in != null) {
+					try {
+						in.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+
+			} catch (ClientProtocolException e) {
+				// TODO Auto-generated catch block
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+			}
+
+			return null;
+		}
+
+		protected Void onPostExecute() {
+			doRobustUpload();
+			return null;
+		}
+
+	}
+
 	private class uploadTask extends AsyncTask<Void, Void, Void> {
 		@Override
 		protected Void doInBackground(Void... params) {
 			long start = System.currentTimeMillis();
-			doRobustUpload();
-			long end = System.currentTimeMillis(); 
-			System.out.println("Execution speed "+ (end - start)+" ms\n"); 
+			new getLineTask().execute();
+			long end = System.currentTimeMillis();
+			System.out.println("Execution speed " + (end - start) + " ms\n");
 			return null;
 		}
 	}
 
 	private void doRobustUpload() {
 
+		String dirPath = Environment.getExternalStorageDirectory().getPath()
+				+ "/robust_upload/";
+
+		File dir = new File(dirPath);
+		File[] fileList = dir.listFiles();
+
+		for (File f : fileList) {
+			doUploadFile(f, 1);
+		}
+
+	}
+
+	private void doUploadFile(File f, int line) {
+
 		HttpURLConnection conn = null;
-		//HttpsURLConnection conn = null;
 		DataOutputStream dos = null;
 		DataInputStream inStream = null;
 
-		String dirPath = "/sdcard/robust_upload/";
-		//String dirPath = "/sdcard/sensordata/";
-		File dir = new File(dirPath);
-		File[] fileList = dir.listFiles();
+		String urlString = "http://dolan.bounceme.net/robust_upload.php";
+		// if
+		// i'm
+		// at
+		// work
+		// and
+		// dolan's
+		// at
+		// home
+		// String urlString = "http://192.168.206.31/robust_upload.php";
+		// //dolan's at work
+		// String urlString = "http://192.168.1.45/robust_upload.php"; //both at
+		// home
 
 		String lineEnd = "\r\n";
 		String twoHyphens = "--";
 		String boundary = "*****";
 
-		int bytesRead, bytesAvailable, bufferSize;
-		byte[] buffer;
-		int maxBufferSize = 1 * 1024 * 1024;
-		// String responseFromServer = "";
-
-		String urlString = "http://dolan.bounceme.net/robust_upload.php"; //if i'm at work and dolan's at home
-		//String urlString = "http://192.168.206.31/robust_upload.php"; //dolan's at work
-		//String urlString = "http://192.168.1.45/robust_upload.php"; //both at home
-
+		/*
+		 * int bytesRead, bytesAvailable, bufferSize; byte[] buffer; int
+		 * maxBufferSize = 1 * 1024 * 1024;
+		 */
 
 		Scanner file_scanner = null;
 
-		for (File f : fileList) {
-			
-			
+		try {
+			file_scanner = new Scanner(f);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-			try {
-				file_scanner = new Scanner (f);
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		while (file_scanner.hasNext()) {
+
+			if (!hasActiveInternetConnection()) {
+				// write code to pause here
+
 			}
 
-			while (file_scanner.hasNext()) {
-				
-				if (!hasActiveInternetConnection()) {
-					//write code to pause here
-					
+			String currentline = null;
+
+			for (int i = 0; i < 10; i++) {
+				currentline += file_scanner.next();
+			}
+
+			Log.d("GB", currentline);
+
+			// upload each line
+			try {
+				// ------------------ CLIENT REQUEST
+				Log.d("MediaPlayer", "Inside second Method");
+
+				// open a URL connection to the Servlet
+				URL url = new URL(urlString);
+
+				// Open a HTTP connection to the URL
+				conn = (HttpURLConnection) url.openConnection();
+
+				// Allow Inputs
+				conn.setDoInput(true);
+
+				// Allow Outputs
+				conn.setDoOutput(true);
+
+				// Don't use a cached copy.
+				conn.setUseCaches(false);
+
+				// Use a post method.
+				conn.setRequestMethod("POST");
+				conn.setRequestProperty("Connection", "Keep-Alive");
+				// System.setProperty("http.keepAlive", "false");
+				conn.setRequestProperty("Content-Type",
+						"multipart/form-data;boundary=" + boundary);
+
+				dos = new DataOutputStream(conn.getOutputStream());
+
+				// write header
+				String key = "u";
+				dos.writeBytes(twoHyphens + boundary + lineEnd);
+				dos.writeBytes("Content-Disposition: form-data; name=" + key
+						+ lineEnd);
+				dos.writeBytes(lineEnd);
+				dos.writeBytes(currentline);
+				dos.writeBytes(lineEnd);
+
+				dos.flush();
+				dos.close();
+
+			} catch (MalformedURLException ex) {
+				Log.d("MediaPlayer", "error: " + ex.getMessage(), ex);
+			}
+
+			catch (IOException ioe) {
+				Log.d("MediaPlayer", "error: " + ioe.getMessage(), ioe);
+			}
+
+			// get server response
+			try {
+				inStream = new DataInputStream(conn.getInputStream());
+				String str;
+
+				while ((str = inStream.readLine()) != null) {
+					Log.d("MediaPlayer", "Server Response: " + str);
 				}
 
-				String currentline = file_scanner.next();
+				inStream.close();
+			}
 
-				Log.d("GB", currentline);
-
-				//upload each line
-				try {
-					// ------------------ CLIENT REQUEST
-					Log.d("MediaPlayer", "Inside second Method");
-
-					// open a URL connection to the Servlet
-					URL url = new URL(urlString);
-
-					// Open a HTTP connection to the URL
-					conn = (HttpURLConnection) url.openConnection();
-
-
-					// Allow Inputs
-					conn.setDoInput(true);
-
-					// Allow Outputs
-					conn.setDoOutput(true);
-
-					// Don't use a cached copy.
-					conn.setUseCaches(false);
-
-					// Use a post method.
-					conn.setRequestMethod("POST");
-					conn.setRequestProperty("Connection", "Keep-Alive");
-					// System.setProperty("http.keepAlive", "false");
-					conn.setRequestProperty("Content-Type",
-							"multipart/form-data;boundary=" + boundary);
-
-					dos = new DataOutputStream(conn.getOutputStream());
-
-					//write header
-					String key = "u";
-					dos.writeBytes(twoHyphens + boundary + lineEnd);
-					dos.writeBytes("Content-Disposition: form-data; name="
-							+ key + lineEnd);
-					dos.writeBytes(lineEnd);
-					dos.writeBytes(currentline);
-					dos.writeBytes(lineEnd);
-
-					dos.flush();
-					dos.close();					
-
-				}
-				catch (MalformedURLException ex) {
-					Log.d("MediaPlayer", "error: " + ex.getMessage(), ex);
-				}
-
-				catch (IOException ioe) {
-					Log.d("MediaPlayer", "error: " + ioe.getMessage(), ioe);
-				}
-
-				//get server response
-				try {
-					inStream = new DataInputStream(conn.getInputStream());
-					String str;
-
-					while ((str = inStream.readLine()) != null) {
-						Log.d("MediaPlayer", "Server Response: " + str);
-					}
-
-					inStream.close();
-				}
-
-				catch (IOException ioex) {
-					Log.d("MediaPlayer", "error: " + ioex.getMessage(), ioex);
-				}
+			catch (IOException ioex) {
+				Log.d("MediaPlayer", "error: " + ioex.getMessage(), ioex);
 			}
 		}
 
 		Log.d("MediaPlayer", "Done uploading everything");
-
 	}
-	
-	
+
 	public boolean hasActiveInternetConnection() {
-		//boolean networkavailable = isNetworkAvailable();
-		//if (networkavailable) {
-			try {
-				HttpURLConnection urlc = (HttpURLConnection) (new URL("http://www.google.com").openConnection());
-				urlc.setRequestProperty("User-Agent", "Test");
-				urlc.setRequestProperty("Connection", "close");
-				urlc.setConnectTimeout(2000); 
-				urlc.connect();
-				if (urlc.getResponseCode() == 200){
-					Log.d("GB", "there is internet");
-					return true;
-				}
-				else {
-					Log.d("GB", "there is no internet");
-				}
-			} catch (IOException e) {
-				Log.e("GB", "Error checking internet connection", e);
+		// boolean networkavailable = isNetworkAvailable();
+		// if (networkavailable) {
+		try {
+			HttpURLConnection urlc = (HttpURLConnection) (new URL(
+					"http://www.google.com").openConnection());
+			urlc.setRequestProperty("User-Agent", "Test");
+			urlc.setRequestProperty("Connection", "close");
+			urlc.setConnectTimeout(2000);
+			urlc.connect();
+			if (urlc.getResponseCode() == 200) {
+				Log.d("GB", "there is internet");
+				return true;
+			} else {
+				Log.d("GB", "there is no internet");
 			}
-		//} else {
-		//	Log.d("GB", "No network available!");
-		//}
+		} catch (IOException e) {
+			Log.e("GB", "Error checking internet connection", e);
+		}
+		// } else {
+		// Log.d("GB", "No network available!");
+		// }
 		return false;
 	}
-	
-}
 
+}
